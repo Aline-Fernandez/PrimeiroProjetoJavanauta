658c781d7335cbec21b307d55e35ec9efd676b31
Aline Fernandes <fernandes.dev@outlook.com>
2026-02-27 10:58:59 -0300

    Operadores aritméticos

---
+++ src/OperadoresAritmeticos.java
@@ -0,0 +1,18 @@
+public class OperadoresAritmeticos {
+
+        public static void main(String[] args) {
+            int idade = 25;
+            long quantidadeDeCachorros = 10L;
+            double salarioMensal = 4500.00;
+            char genero = 'F';
+            boolean empregadoJava = true;
+
+            System.out.println("idade: " + idade);
+            System.out.println("Salário: " + salarioMensal);
+            System.out.println("Gênero: " + genero);
+            System.out.println("Empregado: " + empregadoJava);
+            System.out.println("Cachorros: " + quantidadeDeCachorros);
+
+        }
+    }
+
