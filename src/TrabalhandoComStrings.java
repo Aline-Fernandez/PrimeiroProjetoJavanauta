f4a5399009ed761637ac2006167b88c4f30d2863
Aline Fernandes <fernandes.dev@outlook.com>
2026-02-27 11:00:17 -0300

    Trabalhando com String

---
+++ src/TrabalhandoComStrings.java
@@ -0,0 +1,15 @@
+public class TrabalhandoComStrings {
+    public static void main(String[] args) {
+  String nome = "Aline ";
+  String sobrenome = "Fernandes";
+
+  int tamanhoString = nome.length();
+  boolean saoIguais = nome.equals(sobrenome);
+  String nomeCompleto = nome.concat(sobrenome);
+
+
+        System.out.println("Olá " + nome.toUpperCase() + " Seu nome tem " + tamanhoString + " caracteres");
+        System.out.println("Os nome são iguais: " + saoIguais);
+        System.out.println("Nome completo: " + nomeCompleto);
+    }
+}
